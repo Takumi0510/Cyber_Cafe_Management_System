f27017d5ce8fd85614a56f9d6d5f0da372dfa0c3
Ryan <63656160+Ryan-Lu-Taylors@users.noreply.github.com>
2020-04-16 13:00:29 +0800

    Update NewAccount.h

--- Login_Process_Module/NewAccount.h
+++ Login_Process_Module/NewAccount.h
@@ -1,24 +1,24 @@
 #ifndef NEWACCOUNT_H
 #define NEWACCOUNT_H
 
-#include <string>
+#include <string>				// This includes the string file
 
 using namespace std;
 
 class NewAccount
 {
 	private:
-		string firstName;
+		string firstName;		// Starting from here is the declaration of variables
 		string lastName;
 		int year;
 		int month;
 		int day;
 		
 	public:
-		void getFirstName();
-		void getLastName();
-		void welcomeSentence();
-		int getAge();
+		void getFirstName();		// this is for prompting user first name
+		void getLastName();		// this is for prompting user last name
+		void welcomeSentence();		// this is an individual welcome screen to display the name of user
+		int getAge();			// this is for prompting user age
 		
 };
 
